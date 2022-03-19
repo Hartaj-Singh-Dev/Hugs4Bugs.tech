41e932ac529a6c3ebc2bcf87f0dbc1da5380cdf5
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2022-03-19 11:11:33 +0530

    testing twitter share button

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import {marked} from "marked"
 import Link from "next/link"
+import {useRouter} from 'next/router';
 import React from 'react';
 import fs from 'fs';
 import matter from 'gray-matter';
@@ -38,6 +39,10 @@ interface postPageProps {
 const PostPage:NextPage<postPageProps>  = ({frontmatter , content}) => {
 	const commentBox = React.createRef<any>()
 
+	const {asPath} = useRouter();
+	
+	console.log(asPath)
+
 	React.useEffect(()=>{
 		const scriptEl = document.createElement("script");
 		scriptEl.setAttribute("src","https://utteranc.es/client.js")
@@ -59,8 +64,8 @@ const PostPage:NextPage<postPageProps>  = ({frontmatter , content}) => {
 		 //@ts-ignore
 		 <meta name="Keywords" content={frontmatter.tags?.map((tag)=>{return tag as String}).toString()} />
 	 </Head>
-	 <section className='w-full mt-24 flex justify-evenly '>
-	 <Link href={"/blogs"}><h2 className="hidden  md:block md:sticky md:top-40 md:left-8 cursor-pointer text-[#5790d6] font-bold mt-24 text-xl  underline underline-offset-0">&#60;- Get Back ../</h2></Link>  
+	 <section className="w-full mt-24 flex justify-evenly ">
+	 <Link href={"/blogs"}><h2 className="hidden md:block md:sticky md:top-40 md:left-8 cursor-pointer text-[#5790d6] font-bold mt-24 text-xl  underline underline-offset-0">&#60;- Get Back ../</h2></Link>  
 
 		 <article className="w-full flex md:w-[70%] flex-col justify-items-center items-center md:items-start">
 			 <div className='w-5/6 md:mt-10 sm:w-3/5 md:w-3/4'>
@@ -72,6 +77,12 @@ const PostPage:NextPage<postPageProps>  = ({frontmatter , content}) => {
 
 		 
 		 	</div>		
+
+		<div className="hover:brightness-90">
+		<Link href={`https://twitter.com/intent/tweet?url=https://hugs4-bugs-tech.vercel.app/${asPath}`}>
+			 <i className="cursor-pointer fa-brands fa-twitter fa-2x text-[#1DA1F2]"></i>
+		</Link>
+		</div>	
    
             <div className='h-[1.5px] w-[85%] mt-10 sm:w-[80%] md:w-[75%] bg-white opacity-80'></div>
                <section className=' mt-7 py-8  w-[90%] md:w-full sm:w-full justify-items-center items-center md:items-start overflow-scroll' id='comment'>
