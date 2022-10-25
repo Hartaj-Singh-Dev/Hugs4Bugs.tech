5c75a6b9af0f84b7a8d3f3f23880d97048672554
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2022-10-25 12:10:24 +0530

    making some final changes

--- pages/team.jsx
+++ pages/team.jsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion"
 import Team from "../components/Team/Team.tsx"
-import Navbar from "../components/Navbar"
 import Head from "next/head"
 
 const team = () => {
@@ -14,8 +13,6 @@ const team = () => {
 		<meta name="robots" content="index , follow"/>
 		<meta name="viewport" content="width=device-width , initial-scale=1.0" />
 	 </Head>
-	{/* <Navbar/> */}
-
   <motion.main transition={{ type: 'linear' }} initial={{ opacity: 0, x: -200, y: 0 }} animate={{opacity: 1, x: 0, y: 0}} exit={{ opacity: 0, x: 0, y: -100 }}>
 	 <Team/> 
    </motion.main> 	
